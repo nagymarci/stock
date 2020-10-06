67c7264e33b7bebc7aaa75c127987f47b58c017e
Marton Nagy <marton.nagy@logmein.com>
2020-10-06 22:03:10 +0200

    add simple table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <table>
+      <tr>
+        <th>Symbol</th>
+        <th>Price</th>
+      </tr>
+      <Stock />
+    </table>
+  )
 }
+
+class Stock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isFailed: false
+    }
+  }
+  componentDidMount() {
+    fetch("https://stock.nagymarci.hu/stocks/INTC/calculated")
+    .then((res) => Promise.all([res.status, res.json()]))
+    .then(([code, stockInfo]) => {
+      if (code === 200) {
+        console.log(stockInfo)
+        this.setState({...stockInfo, isFailed: false})
+      } else {
+        this.setState({isFailed: true})
+      }
+    })
+  }
+
+  render() {
+    if (this.state.isFailed) {
+      return (
+        <div>Error during query!</div>
+      )
+    }
+    return (
+      <tr>
+        <td>{this.state.ticker}</td>
+        <td>{this.state.price}</td>
+      </tr>
+    )
+  }
+}
+
+
 
 export default App;
