48d5ea726ff8a56792083b7c4f624eeca920628f
Marton Nagy <marton.nagy@logmein.com>
2020-10-08 21:26:54 +0200

    separate functionalities to pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import StockPage from './StockPage.js';
 import WatchLists from './WatchLists';
 import config from "./config.json";
@@ -7,12 +13,37 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <Admin />
-      <WatchLists />
-      {//<StockPage url={config.baseUrl + "/stocks/calculated"}/>
-      }
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/watchlist">WatchLists</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/watchlist">
+            <WatchLists />
+          </Route>
+          <Route path="/">
+            <StockPage url={config.baseUrl + "/stocks/calculated"}/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
